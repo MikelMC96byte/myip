d36ae8706a77c0f60ae5902ad76e1ba61d0852eb
Mikel Maldonado <71812437+MikelMC96byte@users.noreply.github.com>
2024-04-30 17:33:35 +0200

    feat: get geolocation data

--- src/main.go
+++ src/main.go
@@ -5,17 +5,100 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"os"
 )
 
 type NetworkData struct {
 	IP string `json:"ip"`
 }
 
+type GeoData struct {
+	City struct {
+		Names struct {
+			En string `json:"en"`
+		}
+	}
+	Postal struct {
+		Code string `json:"Code"`
+	}
+	Continent struct {
+		Names struct {
+			En string `json:"en"`
+		}
+		Code string
+	}
+	Subdivisions []struct {
+		Names struct {
+			En string `json:"en"`
+		}
+	}
+	Country struct {
+		Names struct {
+			En string `json:"en"`
+		}
+		IsoCode           string `json:"IsoCode"`
+		GeoNameID         int    `json:"GeoNameID"`
+		IsInEuropeanUnion bool   `json:"IsInEuropeanUnion"`
+	}
+	Location struct {
+		TimeZone       string  `json:"TimeZone"`
+		Latitude       float64 `json:"Latitude"`
+		Longitude      float64 `json:"Longitude"`
+		MetroCode      int     `json:"MetroCode"`
+		AccuracyRadius int     `json:"AccuracyRadius"`
+	}
+}
+
+type Data struct {
+	NetworkData
+	GeoData
+}
+
 const SERVER_URL = "http://mikelmc.dev:3000"
 
 func main() {
 
-	// Makes a get request to the SERVER_URL
+	// Gets argument from command line
+	// if --geo or -g is passed, get the geo location on /geo endpoint
+	// else, get the public IP on / endpoint
+
+	if len(os.Args) > 1 {
+		if os.Args[1] == "--geo" || os.Args[1] == "-g" {
+			data := GeoData{}
+			getGeoData(&data)
+			j, _ := json.MarshalIndent(data, "", "  ")
+			fmt.Println(string(j))
+			os.Exit(0)
+		}
+	}
+	data := NetworkData{}
+	getNetworkData(&data)
+	j, _ := json.MarshalIndent(data, "", "  ")
+	fmt.Println(string(j))
+}
+
+func getGeoData(data *GeoData) {
+	resp, err := http.Get(SERVER_URL + "/geo")
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer resp.Body.Close()
+	body, err := io.ReadAll(resp.Body)
+
+	if err != nil {
+		panic(err)
+	}
+
+	err = json.Unmarshal(body, &data)
+
+	if err != nil {
+		panic(err)
+	}
+}
+
+func getNetworkData(data *NetworkData) {
 	resp, err := http.Get(SERVER_URL)
 
 	if err != nil {
@@ -23,24 +106,15 @@ func main() {
 	}
 
 	defer resp.Body.Close()
-
-	// Reads the response body
-
 	body, err := io.ReadAll(resp.Body)
 
 	if err != nil {
 		panic(err)
 	}
 
-	// Creates a NetworkData struct
-	data := NetworkData{}
-
-	// Unmarshals the response body into the NetworkData struct
 	err = json.Unmarshal(body, &data)
 
 	if err != nil {
 		panic(err)
 	}
-	// Prints the response body
-	fmt.Println("Your public IP address is:", data.IP)
 }
